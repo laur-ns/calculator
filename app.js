640b32fc548aa7bf8123f2afe966884eb39979e3
Laurens <laurens-morales@outlook.com>
2021-12-18 10:09:17 +1300

    Implement input

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
+displayNumber = document.querySelector('.displayNumber');
 
-function add(numOne, numTwo) {
-  return numOne + numTwo;
-}
-function subtract(numOne, numTwo) {
-	return numOne - numTwo;
-}
-function multiply(numOne, numTwo) {
-  return numOne * numTwo;
-}
-function divide(numOne, numTwo) {
-  return numOne / numTwo;
-}
-function modula(numOne, numTwo) {
-  return numOne % numTwo;
-}
-function power(numOne, numTwo) {
-  return numOne ** numTwo;
-}
+function add(numOne, numTwo) { return numOne + numTwo; }
+function subtract(numOne, numTwo) { return numOne - numTwo; }
+function multiply(numOne, numTwo) { return numOne * numTwo; }
+function divide(numOne, numTwo) { return numOne / numTwo; }
+function modulo(numOne, numTwo) { return numOne % numTwo; }
+function power(numOne, numTwo) { return numOne ** numTwo; }
 function operate(numOne, numTwo, operator) {
   if (operator === 'add') { add(numOne, numTwo); }
   if (operator === 'subtract') { subtract(numOne, numTwo); }
   if (operator === 'multiply') { multiply(numOne, numTwo); }
   if (operator === 'divide') { divide(numOne, numTwo); }
-  if (operator === 'modula') { modula(numOne, numTwo); }
+  if (operator === 'modulo') { modulo(numOne, numTwo); }
   if (operator === 'power') { power(numOne, numTwo); }
+}
+
+let buttons = document.querySelectorAll('.button');
+buttons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    id = e.target.getAttribute('class');
+    if (id === 'button number') { 
+      displayNumber.textContent += e.target.getAttribute('id')
+    }
+  })
+});
+
+
+function storeOperator(input) {
+  console.log(input)
 }

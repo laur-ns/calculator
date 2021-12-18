49e6bcf441555df63f05587c7342b989378a38f3
Laurens <laurens-morales@outlook.com>
2021-12-18 23:36:38 +1300

    Calculator finsihed (very ugly code)

--- app.js
+++ app.js
@@ -1,75 +1,38 @@
+// -- Declarations -- //
 let numberDisplay = document.querySelector('.numberDisplay');
 let historyDisplay = document.querySelector('.history');
 let operatorDisplay = document.querySelector('.operator');
-let displayToggle = false; // determines whether to clear display on input
-
+let clearDisplayOnInput = false;
 let firstOperand = '';
 let secondOperand = '';
 let operator = '';
 
 const operations = {
-add: (numOne, numTwo) => { return numOne + numTwo; },
-subtract: (numOne, numTwo) => { return numOne - numTwo; },
-multiply: (numOne, numTwo) => { return numOne * numTwo; },
-divide: (numOne, numTwo) => { return numOne / numTwo; },
-modulo: (numOne, numTwo) => { return numOne % numTwo; },
-power: (numOne, numTwo) => { return numOne ** numTwo; },
-// equals: (
-};
+  add: (numOne, numTwo) => { return numOne + numTwo; },
+  subtract: (numOne, numTwo) => { return numOne - numTwo; },
+  multiply: (numOne, numTwo) => { return numOne * numTwo; },
+  divide: (numOne, numTwo) => { return numOne / numTwo; },
+  modulo: (numOne, numTwo) => { return numOne % numTwo; },
+  power: (numOne, numTwo) => { return numOne ** numTwo; },
+}
 
-// -- functions -- //
+
+// -- Functions -- //
 function operate(numOne, numTwo, operator) {
-  numOne = parseInt(numOne);
-  numTwo = parseInt(numTwo);
+  numOne = Number(numOne);
+  numTwo = Number(numTwo);
+  if (operator === 'equals') {
+    changeDisplay('clearMain')
+  }
+  changeDisplay('showEquation', operator);
   firstOperand = operations[operator](numOne, numTwo);
   numberDisplay.textContent = firstOperand;
-  changeDisplay('showEquation', operator);
-  displayToggle = true;
+  clearDisplayOnInput = true;
   secondOperand = '';
 }
-let buttons = document.querySelectorAll('.button');
-buttons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    let id = e.target.getAttribute('id');
-    let classes = e.target.getAttribute('class');
-    // check if there is already a decimal point
-    if ((id === '.') &&
-        numberDisplay.textContent.includes('.')) {
-      return;
-    }
-    // check if button pressed is a number
-    if (classes === 'button number') { 
-      if (displayToggle) {
-        numberDisplay.textContent = '';
-        displayToggle = false;
-        secondOperand = '';
-      }
-      numberDisplay.textContent += id;
-    }
-    // check if button pressed is an operator
-    if (classes.includes('operator')) {
-      if (displayToggle) {
-        operator = id;
-        changeDisplay('clearMain', id);
-        return;
-      }
-      if (firstOperand === '') {
-        firstOperand = numberDisplay.textContent;
-        operator = id;
-        changeDisplay('clearMain', id);
-        return;
-      }
-      secondOperand = numberDisplay.textContent;
-      if (secondOperand === '') { return };  
-      changeDisplay('clearMain', id);
-      operate(firstOperand, secondOperand, operator);
-      operator = id;
-    }
-  })
-});
-
 function changeDisplay(displayType, operator) {
   let operatorSymbol =  document.querySelector(`#${operator}`).textContent;
+  if (operator === 'power') { operatorSymbol = '^'; }
   if (displayType === 'clearMain') {
     operatorDisplay.textContent = operatorSymbol;
     historyDisplay.textContent = firstOperand;
@@ -84,3 +47,56 @@ function changeDisplay(displayType, operator) {
     historyDisplay.textContent = `${firstOperand} ${operatorSymbol} ${secondOperand}`;
   }
 }
+
+
+// -- Add event listeners --//
+let buttons = document.querySelectorAll('.button');
+buttons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    let id = e.target.getAttribute('id');
+    let classes = e.target.getAttribute('class');
+
+    // check if there is already a decimal point
+    if ((id === '.') &&
+        numberDisplay.textContent.includes('.')) {
+      return;
+    }
+
+    // check if button pressed is a number
+    if (classes === 'button number') { 
+      if (operator === 'equals') {
+        firstOperand = '';
+        changeDisplay('clearAll', operator);
+      }
+      if (clearDisplayOnInput) {
+        numberDisplay.textContent = '';
+        clearDisplayOnInput = false;
+        secondOperand = '';
+      }
+      numberDisplay.textContent += id;
+    }
+
+    // check if button pressed is an operator
+    if (classes.includes('operator')) {
+      if (clearDisplayOnInput) {
+        operator = id;
+        changeDisplay('clearMain', id);
+        return;
+      }
+      if (firstOperand === '') {
+        firstOperand = numberDisplay.textContent;
+        operator = id;
+        changeDisplay('clearMain', id);
+        return;
+      }
+      secondOperand = numberDisplay.textContent;
+      if (secondOperand === '') {
+        changeDisplay('clearMain', id)
+        return;
+      };  
+      changeDisplay('clearMain', id);
+      operate(firstOperand, secondOperand, operator);
+      operator = id;
+    }
+  })
+});

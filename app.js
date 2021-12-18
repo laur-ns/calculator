90e06446b828b6348df80adab58f01adbfd61cfe
Laurens <laurens-morales@outlook.com>
2021-12-18 20:18:31 +1300

    Add operator symbol on display

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-let displayNumber = document.querySelector('.displayNumber');
+let numberDisplay = document.querySelector('.numberDisplay');
+let historyDisplay = document.querySelector('.history');
+let operatorDisplay = document.querySelector('.operator');
 
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+
+// -- functions
 function add(numOne, numTwo) { return numOne + numTwo; }
 function subtract(numOne, numTwo) { return numOne - numTwo; }
 function multiply(numOne, numTwo) { return numOne * numTwo; }
@@ -7,35 +14,55 @@ function divide(numOne, numTwo) { return numOne / numTwo; }
 function modulo(numOne, numTwo) { return numOne % numTwo; }
 function power(numOne, numTwo) { return numOne ** numTwo; }
 function operate(numOne, numTwo, operator) {
-  if (operator === 'add') { add(numOne, numTwo); }
-  if (operator === 'subtract') { subtract(numOne, numTwo); }
-  if (operator === 'multiply') { multiply(numOne, numTwo); }
-  if (operator === 'divide') { divide(numOne, numTwo); }
-  if (operator === 'modulo') { modulo(numOne, numTwo); }
-  if (operator === 'power') { power(numOne, numTwo); }
+  console.log(numOne, numTwo, operator)
+  secondOperand = '';
+  // if (operator === 'add') { add(numOne, numTwo); }
+  // if (operator === 'subtract') { subtract(numOne, numTwo); }
+  // if (operator === 'multiply') { multiply(numOne, numTwo); }
+  // if (operator === 'divide') { divide(numOne, numTwo); }
+  // if (operator === 'modulo') { modulo(numOne, numTwo); }
+  // if (operator === 'power') { power(numOne, numTwo); }
 }
-
 let buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
-  // 
   button.addEventListener('click', (e) => {
     let id = e.target.getAttribute('id');
-    let className = e.target.getAttribute('class');
+    let classes = e.target.getAttribute('class');
     // check if there is already a decimal point
-    if ((e.target.getAttribute('id') === '.') &&
-        displayNumber.textContent.includes('.')) {
-          return
+    if ((id === '.') &&
+        numberDisplay.textContent.includes('.')) {
+      return;
     }
     // check if button pressed is a number
-    if (className === 'button number') { 
-      displayNumber.textContent += e.target.getAttribute('id')
+    if (classes === 'button number') { 
+      numberDisplay.textContent += id;
     }
     // check if button pressed is an operator
-    if (id === multiply) { return }
+    if (classes.includes('operator')) {
+      if (firstOperand == '') {
+        firstOperand = numberDisplay.textContent;
+        operator = id;
+        changeDisplay('main', id);
+        return;
+      }
+      secondOperand = numberDisplay.textContent;
+      changeDisplay('main', id);
+      operate(firstOperand, secondOperand, operator);
+      operator = id;
+    }
   })
 });
 
-
-function storeOperator(input) {
-  console.log(input)
+function changeDisplay(displayType, operator) {
+  let operatorSymbol =  document.querySelector(`#${operator}`).textContent;
+  if (displayType === 'main') {
+    operatorDisplay.textContent = operatorSymbol;
+    historyDisplay = numberDisplay.textContent;
+    numberDisplay.textContent = ' ';
+  }
+  if (displayType === 'all') {
+    historyDisplay.textContent = ''
+    numberDisplay.textContent = '';
+    operatorDisplay.textContent = '';
+  }
 }
